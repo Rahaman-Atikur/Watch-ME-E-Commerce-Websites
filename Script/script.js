be15ea107bddfe6ea564ee02cfa2425db40ec3ee
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-08 23:43:42 +0100

    functionality done

--- Script/script.js
+++ Script/script.js
@@ -2,8 +2,16 @@ const ringButtons = document.querySelectorAll('.ring-button');
 for(let i=0;i<ringButtons.length;i++){
     const ringBtn = ringButtons[i];
     ringBtn.addEventListener("click",function (event){
-        event.target.classList.add('border-purple-600');
         
-    })
+        for(let j=0;j<ringButtons.length;j++){
+            ringButtons[j].classList.remove('border-purple-600');
+            ringButtons[j].classList.add("border-gray-300");
+        }
+         event.target.classList.add('border-purple-600');
+         event.target.classList.remove('border-purple-600');
+         const productImage = document.getElementById("product-image");
+         productImage.src='../images/cyan.png';
+    });
+    
 
 }
