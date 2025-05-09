2732bc0e4f73389b3e3228abed6e27642f2fc221
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-09 08:39:26 +0100

    Recoded something

--- Script/script.js
+++ Script/script.js
@@ -1,17 +1,25 @@
-const ringButtons = document.querySelectorAll('.ring-button');
-for(let i=0;i<ringButtons.length;i++){
-    const ringBtn = ringButtons[i];
-    ringBtn.addEventListener("click",function (event){
+// const ringButtons = document.querySelectorAll('.ring-button');
+// for(let i=0;i<ringButtons.length;i++){
+//     const ringButton = ringButtons[i];
+//     ringButton.addEventListener("click",function (event){
         
-        for(let j=0;j<ringButtons.length;j++){
-            ringButtons[j].classList.remove('border-purple-600');
-            ringButtons[j].classList.add("border-gray-300");
-        }
-         event.target.classList.add('border-purple-600');
-         event.target.classList.remove('border-purple-600');
-         const productImage = document.getElementById("product-image");
-         productImage.src='../images/cyan.png';
-    });
+
+
+
+
+
+
+        
+//         for(let j=0;j<ringButtons.length;j++){
+//             ringButtons[j].classList.remove('border-purple-600');
+//             ringButtons[j].classList.add("border-gray-300");
+//         }
+//          event.target.classList.add('border-purple-600');
+//         //  event.target.classList.remove('border-purple-600');
+         
+//          const productImage = document.getElementById("product-image");
+//          productImage.src='../images/cyan.png';
+//     });
     
 
-}
+// }
