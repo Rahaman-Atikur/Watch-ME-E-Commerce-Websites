6aa28a936da742597e533978a2534c2327144634
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-11 01:22:20 +0100

    Button is not pic changing

--- Script/script.js
+++ Script/script.js
@@ -1,25 +1,17 @@
-// const ringButtons = document.querySelectorAll('.ring-button');
-// for(let i=0;i<ringButtons.length;i++){
-//     const ringButton = ringButtons[i];
-//     ringButton.addEventListener("click",function (event){
-        
+const ringButtons = document.querySelectorAll(".ring-button");
+for (let i = 0; i < ringButtons.length; i++) {
+  const ringButton = ringButtons[i];
+  ringButton.addEventListener("click", function (event) {
+    const color = event.target.id.replace("-color", " ");
+    for (let j = 0; j < ringButtons.length; j++) {
+      ringButtons[j].classList.remove("border-purple-600");
+      ringButtons[j].classList.add("border-gray-300");
+    }
+    event.target.classList.add("border-purple-600");
+    event.target.classList.remove("border-gray-300");
 
-
-
-
-
-
-        
-//         for(let j=0;j<ringButtons.length;j++){
-//             ringButtons[j].classList.remove('border-purple-600');
-//             ringButtons[j].classList.add("border-gray-300");
-//         }
-//          event.target.classList.add('border-purple-600');
-//         //  event.target.classList.remove('border-purple-600');
-         
-//          const productImage = document.getElementById("product-image");
-//          productImage.src='../images/cyan.png';
-//     });
+    const productImage = document.getElementById("product-image");
     
-
-// }
+    productImage.src ="../images/" + color + ".png";
+  });
+}
