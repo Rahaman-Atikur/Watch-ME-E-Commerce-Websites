93aa1dc3867e6000a5c7fc3368c16e637aaca6c5
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-11 18:52:13 +0100

    solved

--- Script/script.js
+++ Script/script.js
@@ -2,7 +2,7 @@ const ringButtons = document.querySelectorAll(".ring-button");
 for (let i = 0; i < ringButtons.length; i++) {
   const ringButton = ringButtons[i];
   ringButton.addEventListener("click", function (event) {
-    const color = event.target.id.replace("-color", " ");
+    const color = event.target.id.replace("-color", "");
     for (let j = 0; j < ringButtons.length; j++) {
       ringButtons[j].classList.remove("border-purple-600");
       ringButtons[j].classList.add("border-gray-300");
@@ -12,6 +12,6 @@ for (let i = 0; i < ringButtons.length; i++) {
 
     const productImage = document.getElementById("product-image");
     
-    productImage.src ="../images/" + color + ".png";
+   productImage.src ="../images/"+color+".png"; 
   });
 }
